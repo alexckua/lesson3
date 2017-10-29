546db46c041ab9818c5fad93db9117125a7b12c9
Alex Masliychuk <alexmasliychuk@gmail.com>
2017-10-29 13:34:47 +0200

    homework 3 added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user = User.find_by(token: session[:user_token])
   end
+
+  def gravatar_user_image
+    current_user ? Gravatar.new( current_user.email ).image_url : false
+  end
+
   helper_method :current_user
+  helper_method :gravatar_user_image
 end
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,16 @@
 class ChatsController < ApplicationController
-  def show
+  before_action :require_user
+
+  def chat_messages
+    Message.all
   end
+
+  helper_method :chat_messages
+
+  private
+
+  def require_user
+    redirect_to root_path, notice: 'You\'re not logged! Please login at first.' unless current_user
+  end
+
 end
