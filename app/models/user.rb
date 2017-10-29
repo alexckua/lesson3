84ff492c5ca1bdce498b92dae44ca6bcfbc6a239
Alex Masliychuk <alexmasliychuk@gmail.com>
2017-10-29 13:41:38 +0200

    unique email fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: { message: 'should be defined' }
-  validates :email, presence: { message: 'should be defined' }
-  # , uniqueness: {message: 'should be unique!'}
+  validates :email, presence: { message: 'should be defined' }, uniqueness: {message: 'should be unique!'}
   has_many :messages, dependent: :destroy
   has_secure_token
 end
