d03acfd4595cbd50f68dd944f8da5bc0db87789e
galulex <alex@active-bridge.com>
2017-12-04 19:05:21 +0200

    Refactor search dynamic url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,35 +21,9 @@
 $(document).on('keyup', '#search-messages', function() {
     if ( $(this).val().length > 0 ) {
         Rails.fire(this.form, 'submit');
-        // change parameter in url
-        var newURL = updateURLParameter(window.location.href, 'q', $(this).val());
-        window.history.replaceState('', '', newURL);
     }
 });
 
 function scrollToBottom() {
     $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-}
-
-/**
- * http://stackoverflow.com/a/10997390/11236
- */
-function updateURLParameter(url, param, paramVal){
-    var newAdditionalURL = "";
-    var tempArray = url.split("?");
-    var baseURL = tempArray[0];
-    var additionalURL = tempArray[1];
-    var temp = "";
-    if (additionalURL) {
-        tempArray = additionalURL.split("&");
-        for (var i=0; i<tempArray.length; i++){
-            if(tempArray[i].split('=')[0] != param){
-                newAdditionalURL += temp + tempArray[i];
-                temp = "&";
-            }
-        }
-    }
-
-    var rows_txt = temp + "" + param + "=" + paramVal;
-    return baseURL + "?" + newAdditionalURL + rows_txt;
 }
